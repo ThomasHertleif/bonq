61b5f94ae2c49c14db851ac18a11fba011acac51
Thomas Hertleif <thomas@hertleif.de>
2021-12-24 17:16:44 +0100

    First bonq

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use std::f32::consts::PI;
+
+use bevy::{core::FixedTimestep, prelude::*};
+
+use bevy_inspector_egui::{Inspectable, WorldInspectorPlugin};
+
+const TIME_STEP: f32 = 1.0 / 60.0;
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "bonq".to_string(),
+            width: 600.,
+            height: 800.,
+            vsync: true,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::MIDNIGHT_BLUE))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(setup)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
+                .with_system(launch_ball)
+                .with_system(move_the_ball),
+        )
+        .run();
+}
+
+#[derive(Component, Inspectable)]
+struct Ball;
+
+#[derive(Component, Inspectable, Default)]
+struct NewBall {
+    degree: f32,
+    velocity: f32,
+}
+
+#[derive(Component, Inspectable, Default)]
+struct StickyBall {
+    size: f32,
+}
+
+#[derive(Component, Inspectable, Default)]
+struct Moving {
+    velocity: Vec2,
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    commands.spawn_bundle(SpriteBundle {
+        transform: Transform {
+            translation: Vec3::new(0.0, -250.0, 0.0),
+            scale: Vec3::new(600.0, 2.0, 1.0),
+            ..Default::default()
+        },
+        sprite: Sprite {
+            color: Color::DARK_GREEN,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, -300.0, 0.0),
+                scale: Vec3::new(10.0, 10.0, 1.0),
+                ..Default::default()
+            },
+            sprite: Sprite {
+                color: Color::LIME_GREEN,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Ball)
+        .insert(NewBall {
+            degree: 360.,
+            velocity: 1.,
+        });
+}
+
+fn launch_ball(
+    keyboard_input: Res<Input<KeyCode>>,
+    ball: Query<(Entity, &NewBall), With<NewBall>>,
+    mut commands: Commands,
+) {
+    if keyboard_input.pressed(KeyCode::Space) {
+        if let Ok((new_ball, NewBall { degree, velocity })) = ball.get_single() {
+            info!("very good ball");
+            commands
+                .entity(new_ball)
+                .insert(Moving {
+                    velocity: {
+                        let radians = degree / (180. * PI);
+                        let x = radians.cos() * velocity;
+                        let y = radians.sin() * velocity;
+
+                        Vec2::new(x, y)
+                    },
+                })
+                .remove::<NewBall>();
+        } else {
+            info!("No ball for you");
+        }
+    }
+}
+
+fn move_the_ball(mut balls: Query<(&mut Transform, &Moving), (With<Moving>, With<Ball>)>) {
+    for (mut transform, Moving { velocity }) in balls.iter_mut() {
+        transform.translation.x += velocity.x;
+        transform.translation.y += velocity.y;
+    }
+}
